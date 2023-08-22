b87939f764b5681af9696dc3040e371f8fee654d
VedantK709 <140979889+VedantK709@users.noreply.github.com>
2023-08-22 14:49:26 +0530

    SGGS Vmk PPS pr prob 1

---
+++ vmkpps p1.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+  float r;
+  float area;
+  cout<<"Enter radius : "<<endl;
+  cin>>r;
+  area = 3.14*r*r;
+  cout<<"Area is : "<<area<<" units"<<endl;
+
+  return 0;
+}
+
